0a3e674d33a85db42bf2d6a56c36cd5c2039c489
dennis.sauve <dennis.sauve@triadhq.com>
2018-11-28 19:14:48 -0800

    Getting ready for search bar

--- index.php
+++ index.php
@@ -1,36 +1,37 @@
 <?php
 	require('include/authenticate.php');
-	include('include/newSiteLogic.php'); 
+	include('include/newSiteLogic.php');
 ?>
 <!DOCTYPE html>
 <html>
 	<head>
 		<meta charset="utf-8">
     	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-		
+
 		<title>Dennis' Dash</title>
 		<meta name="description" content="A list of useful links and tools."/>
-		
+
 		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
 		<link rel="stylesheet" href="style/style.css" />
 		<link rel="stylesheet" href="style/tile.css" />
 	</head>
 	<body>
 		<div class="container">
-			
+
 			<div class="row">
 				<a href="/" class="h1 col-6">Dashboard</a>
 				<div class="col-6 text-right">
 					<a href="/vault.php" class="btn btn-secondary" roll="button">meme vault</a>
 					<a href="/tools.php" class="btn btn-secondary" roll="button">tools</a>
 					<a href="/?logout=true" class="btn btn-secondary" roll="button">logout</a>
-				</div>	
+				</div>
 			</div>
-
-			<?php 
-				include('include/newSite.php');
-				include('include/siteCards.php'); 
-			?>
+            <div id="sites">
+			    <?php
+				    include('include/newSite.php');
+				    include('include/siteCards.php');
+			    ?>
+            </div>
 
 		</div>
 
